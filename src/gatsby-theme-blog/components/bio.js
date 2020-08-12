02f6d02f251f13379d8916b6f76e10c87a29fbab
Sukjoon Kim <sjoonk@gmail.com>
2020-08-12 16:41:10 +0900

    update bio

--- src/gatsby-theme-blog/components/bio.js
+++ src/gatsby-theme-blog/components/bio.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Heading } from 'theme-ui'
 
 const Bio = () => (
-  <Heading as='h3'>Here Comes Everyone!</Heading>
+  <Heading as='h3'>Here Comes Everybody!</Heading>
 )
 
 export default Bio
